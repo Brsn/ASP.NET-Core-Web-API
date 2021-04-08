19be2886fd4e9dee0e62001799913bdf96560d9a
Bronson Brown <Brsn90@icloud.com>
2021-04-08 16:43:30 -0600

    Add files via upload

---
+++ Models/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    // the colon here is for inheriting DbContext class
+    public class TodoContext : DbContext
+    {
+        //constructior passing in DbContext options
+        public TodoContext(DbContextOptions<TodoContext> options)
+
+             // : base is used to refer to the base class when chaining constructors
+             : base(options)
+        {
+        }
+            public DbSet<TodoItem> TodoItems { get; set; }
+    }
+    }        
